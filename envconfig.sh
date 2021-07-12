52115287353f091d42bf8de7db6a1b59c9b48812
readall <31568561+readall@users.noreply.github.com>
2021-07-12 20:58:40 +0530

    Update envconfig.sh

--- envconfig.sh
+++ envconfig.sh
@@ -19,6 +19,8 @@ conda install -y -c conda-forge beakerx
 conda install -y -c conda-forge xeus-cling
 conda install -y -c conda-forge xeus-python
 conda install pytorch torchvision torchaudio cpuonly -c pytorch
+conda install -y -c conda-forge tensorflow
+conda install -y -c conda-forge keras
 conda install -c huggingface transformers
 
 # Some extra packages for your environments
